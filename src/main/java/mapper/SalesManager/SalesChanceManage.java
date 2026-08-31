498a42b0d199d234efa7a652731400bde0c48ba4
Yuuki-Okincy <3552057096@qq.com>
2026-08-31 16:23:28 +0800

    实现了销售机会管理

--- src/main/java/mapper/SalesManager/SalesChanceManage.java
+++ src/main/java/mapper/SalesManager/SalesChanceManage.java
@@ -1,6 +1,11 @@
 package mapper.SalesManager;
 
+import entity.SalChance;
+
+//销售主管、客户经理模块的Mapper接口
 public interface SalesChanceManage {
-    int selectSalesChance();
-    int insertSalesChance();
+    //根据客户名称或联系人查询销售机会
+    int selectSalesChanceByNameOrContactPerson(String name, String contactPerson);
+    //插入销售机会
+    int insertSalesChance(SalChance salChance);
 }
